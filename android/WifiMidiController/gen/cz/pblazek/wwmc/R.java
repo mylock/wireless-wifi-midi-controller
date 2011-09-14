021749a8d8d60c8872b70021d4b1df8b101f570b
rtep.kezalb <rtep.kezalb@gmail.com>
2011-09-14 21:32:54 +0000

    creation of resources and activities

--- android/WifiMidiController/gen/cz/pblazek/wwmc/R.java
+++ android/WifiMidiController/gen/cz/pblazek/wwmc/R.java
@@ -12,23 +12,25 @@ public final class R {
     }
     public static final class drawable {
         public static final int app_wwmc=0x7f020000;
-        public static final int ic_menu_music_library=0x7f020001;
+        public static final int ic_menu_back=0x7f020001;
         public static final int ic_menu_settings=0x7f020002;
     }
     public static final class id {
-        public static final int menu_piano_roll=0x7f060000;
+        public static final int menu_back=0x7f060002;
         public static final int menu_settings=0x7f060001;
+        public static final int textView1=0x7f060000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int piano_roll=0x7f030000;
+        public static final int settings=0x7f030001;
     }
     public static final class menu {
-        public static final int options_menu=0x7f050000;
+        public static final int piano_roll_options_menu=0x7f050000;
+        public static final int settings_options_menu=0x7f050001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int menu_piano_roll=0x7f040002;
-        public static final int menu_settings=0x7f040003;
+        public static final int app_name=0x7f040000;
+        public static final int menu_back=0x7f040002;
+        public static final int menu_settings=0x7f040001;
     }
 }
