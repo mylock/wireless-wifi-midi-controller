9ceada8e29ff6388bd06733622baea5a998947c6
rtep.kezalb@gmail.com <rtep.kezalb@gmail.com@25c4ba18-c406-9941-3bff-27d4ade6cb0d>
2011-10-10 00:28:04 +0000

    test udp client

---
+++ trunk/test/UdpClient/src/cz/pblazek/wwmc/test/key/KeyEvent.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2011 The Wireless WiFi MIDI Controller Open Source Project
+ * 
+ * Licensed under the GNU General Public License, Version 3
+ */
+package cz.pblazek.wwmc.test.key;
+
+import java.util.EventObject;
+
+/**
+ * @author rtep.kezalb@gmail.com
+ * 
+ */
+public class KeyEvent extends EventObject {
+
+	private static final long serialVersionUID = -6444379116966221362L;
+
+	public KeyEvent(Object source) {
+		super(source);
+	}
+
+}
---
+++ trunk/test/UdpClient/src/cz/pblazek/wwmc/test/key/PianoKeyEnum.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2011 The Wireless WiFi MIDI Controller Open Source Project
+ * 
+ * Licensed under the GNU General Public License, Version 3
+ */
+package cz.pblazek.wwmc.test.key;
+
+/**
+ * @author rtep.kezalb@gmail.com
+ * 
+ */
+public enum PianoKeyEnum {
+
+	PK_C00("C00", new Key[] { new Key(1, 1, 60, 136, Key.KEY_COLOR_WHITE), new Key(1, 137, 80, 136, Key.KEY_COLOR_WHITE) }),
+
+	PK_CIS00("C#00", new Key[] { new Key(62, 1, 40, 136, Key.KEY_COLOR_BLACK) }),
+
+	PK_D00("D00", new Key[] { new Key(102, 1, 40, 136, Key.KEY_COLOR_WHITE), new Key(82, 137, 80, 136, Key.KEY_COLOR_WHITE) }),
+
+	PK_DIS00("D#00", new Key[] { new Key(143, 1, 40, 136, Key.KEY_COLOR_BLACK) }),
+
+	PK_E00("E00", new Key[] { new Key(183, 1, 60, 136, Key.KEY_COLOR_WHITE), new Key(163, 137, 80, 136, Key.KEY_COLOR_WHITE) }),
+
+	PK_F00("F00", new Key[] { new Key(244, 1, 60, 136, Key.KEY_COLOR_WHITE), new Key(244, 137, 80, 136, Key.KEY_COLOR_WHITE) }),
+
+	PK_FIS00("F#00", new Key[] { new Key(305, 1, 40, 136, Key.KEY_COLOR_BLACK) }),
+
+	PK_G00("G00", new Key[] { new Key(345, 1, 40, 136, Key.KEY_COLOR_WHITE), new Key(325, 137, 80, 136, Key.KEY_COLOR_WHITE) }),
+
+	PK_GIS00("G#00", new Key[] { new Key(386, 1, 40, 136, Key.KEY_COLOR_BLACK) }),
+
+	PK_A00("A00", new Key[] { new Key(426, 1, 40, 136, Key.KEY_COLOR_WHITE), new Key(406, 137, 80, 136, Key.KEY_COLOR_WHITE) }),
+
+	PK_AIS00("A#00", new Key[] { new Key(467, 1, 40, 136, Key.KEY_COLOR_BLACK) }),
+
+	PK_H00("H00", new Key[] { new Key(507, 1, 60, 136, Key.KEY_COLOR_WHITE), new Key(487, 137, 80, 136, Key.KEY_COLOR_WHITE) }),
+
+	PK_C01("C01", new Key[] { new Key(568, 1, 80, 272, Key.KEY_COLOR_WHITE) }),
+
+	;
+
+	private final String tone;
+
+	private final Key[] key;
+
+	private PianoKeyEnum(final String tone, final Key[] key) {
+		this.tone = tone;
+		this.key = key;
+	}
+
+	public String getTone() {
+		return tone;
+	}
+
+	public Key[] getKey() {
+		return key;
+	}
+
+	//
+
+	public static PianoKeyEnum findByTone(String tone) {
+		PianoKeyEnum pianoKeyEnum = null;
+		if (tone != null) {
+			for (PianoKeyEnum value : PianoKeyEnum.values()) {
+				if (tone.startsWith(value.getTone())) {
+					pianoKeyEnum = value;
+					break;
+				}
+			}
+		}
+		return pianoKeyEnum;
+	}
+}
