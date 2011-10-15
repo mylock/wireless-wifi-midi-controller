a2fd631c8571cfdbc336c68a1545aefbd6890a10
rtep.kezalb <rtep.kezalb@gmail.com>
2011-10-15 13:44:07 +0000

    minor changes in controller application and test udp client, initial arduino code, wifly shield setup

--- test/UdpClient/src/cz/pblazek/wwmc/test/key/PianoKeyEnum.java
+++ test/UdpClient/src/cz/pblazek/wwmc/test/key/PianoKeyEnum.java
@@ -13,31 +13,31 @@ import java.awt.Color;
  */
 public enum PianoKeyEnum {
 
-	PK_C00("C00", new Key[] { new Key(1, 1, 60, 136, Color.WHITE), new Key(1, 137, 80, 136, Color.WHITE) }),
+	PK_C00("C00;", new Key[] { new Key(1, 1, 60, 136, Color.WHITE), new Key(1, 137, 80, 136, Color.WHITE) }),
 
-	PK_CIS00("C#00", new Key[] { new Key(62, 1, 40, 136, Color.BLACK) }),
+	PK_CIS00("C#00;", new Key[] { new Key(62, 1, 40, 136, Color.BLACK) }),
 
-	PK_D00("D00", new Key[] { new Key(102, 1, 40, 136, Color.WHITE), new Key(82, 137, 80, 136, Color.WHITE) }),
+	PK_D00("D00;", new Key[] { new Key(102, 1, 40, 136, Color.WHITE), new Key(82, 137, 80, 136, Color.WHITE) }),
 
-	PK_DIS00("D#00", new Key[] { new Key(143, 1, 40, 136, Color.BLACK) }),
+	PK_DIS00("D#00;", new Key[] { new Key(143, 1, 40, 136, Color.BLACK) }),
 
-	PK_E00("E00", new Key[] { new Key(183, 1, 60, 136, Color.WHITE), new Key(163, 137, 80, 136, Color.WHITE) }),
+	PK_E00("E00;", new Key[] { new Key(183, 1, 60, 136, Color.WHITE), new Key(163, 137, 80, 136, Color.WHITE) }),
 
-	PK_F00("F00", new Key[] { new Key(244, 1, 60, 136, Color.WHITE), new Key(244, 137, 80, 136, Color.WHITE) }),
+	PK_F00("F00;", new Key[] { new Key(244, 1, 60, 136, Color.WHITE), new Key(244, 137, 80, 136, Color.WHITE) }),
 
-	PK_FIS00("F#00", new Key[] { new Key(305, 1, 40, 136, Color.BLACK) }),
+	PK_FIS00("F#00;", new Key[] { new Key(305, 1, 40, 136, Color.BLACK) }),
 
-	PK_G00("G00", new Key[] { new Key(345, 1, 40, 136, Color.WHITE), new Key(325, 137, 80, 136, Color.WHITE) }),
+	PK_G00("G00;", new Key[] { new Key(345, 1, 40, 136, Color.WHITE), new Key(325, 137, 80, 136, Color.WHITE) }),
 
-	PK_GIS00("G#00", new Key[] { new Key(386, 1, 40, 136, Color.BLACK) }),
+	PK_GIS00("G#00;", new Key[] { new Key(386, 1, 40, 136, Color.BLACK) }),
 
-	PK_A00("A00", new Key[] { new Key(426, 1, 40, 136, Color.WHITE), new Key(406, 137, 80, 136, Color.WHITE) }),
+	PK_A00("A00;", new Key[] { new Key(426, 1, 40, 136, Color.WHITE), new Key(406, 137, 80, 136, Color.WHITE) }),
 
-	PK_AIS00("A#00", new Key[] { new Key(467, 1, 40, 136, Color.BLACK) }),
+	PK_AIS00("A#00;", new Key[] { new Key(467, 1, 40, 136, Color.BLACK) }),
 
-	PK_H00("H00", new Key[] { new Key(507, 1, 60, 136, Color.WHITE), new Key(487, 137, 80, 136, Color.WHITE) }),
+	PK_H00("H00;", new Key[] { new Key(507, 1, 60, 136, Color.WHITE), new Key(487, 137, 80, 136, Color.WHITE) }),
 
-	PK_C01("C01", new Key[] { new Key(568, 1, 80, 272, Color.WHITE) }),
+	PK_C01("C01;", new Key[] { new Key(568, 1, 80, 272, Color.WHITE) }),
 
 	;
 
